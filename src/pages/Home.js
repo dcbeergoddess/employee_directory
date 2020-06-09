a306bd4e3c1af5b3886fe9cd384482fbf564c735
Rachel Murray <dcbeergoddess@gmail.com>
2020-06-09 09:24:08 -0400

    creating initial pages and structure to fill in functionality

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import API from "../utlis/API";
+
+
+class App extends React.Component {
+  
+    state = {
+        employeeArray : [],
+    }
+    
+    // componentDidMount() {    
+       
+    //     };
+    // } 
+    
+    //NEED API CALL
+
+
+    render() {
+        return(
+            
+            <>
+            
+                <h1>Employee Directory</h1>
+              
+
+            </>
+            )
+        }
+    }
+    
+    export default App;
